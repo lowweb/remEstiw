1251cf07bbf1a85798e62ddd8f8e92e09fc489f0
lowweb <37337132+lowweb@users.noreply.github.com>
2018-12-24 01:21:06 +1000

    stc

--- src/common.blocks/header/header-sticky/header-sticky.js
+++ src/common.blocks/header/header-sticky/header-sticky.js
@@ -1,45 +1,72 @@
 
+$(".main-content").scroll (function () {
+console.log ('двигаем');
+ if ( $(this).scrollTop() > $('.header-currency').height()) {
+
+    $(".header-sticky").addClass("header-sticky--active");
+     $(".header-sticky").attr('style' , "width: calc(100% - " + $('.navbar').width() + "px )" );
+    // $(".navbar-ul").addClass("navbar-ul--understicky");
+
+ 	// $(".header-currency").addClass("header-currency--hide");
+  	// $('.header-sticky')[0].scrollIntoView(true);
+
+}  
+if ( $(this).scrollTop() == 0) {
+
+     $(".header-sticky").removeClass("header-sticky--active");
+    // $(".navbar-ul").removeClass("navbar-ul--understicky");
+  	// $('.header-currency')[0].scrollIntoView(true);
+
+  	// $(".header-currency").removeClass("header-currency--hide");
+ }
+
+ });
+
+
+
 
 // var header = $('.header-sticky');
 // Stickyfill.add(header);
 
 
-$(".view").scroll (function () {
+// $(".main-content").scroll (function () {
 
-// console.log (window.innerHeight, $('.navbar-ul').prop('scrollHeight'));
-//надо обьединить блоки с хедерами чтоб работало
+// // console.log (window.innerHeight, $('.navbar-ul').prop('scrollHeight'));
+// //надо обьединить блоки с хедерами чтоб работало
 
 
-// if (window.innerHeight < $('.navbar-ul').prop('scrollHeight') - 44 && $('.navbar__submenu').hasClass('navbar__submenu--open')) {
-	 console.log ('двигаем');
-// 	 // console.log ('obratno');
-	if ( $(this).scrollTop() > $('.navbar__item-main-menu').height() /*&& $('.navbar__submenu').hasClass('navbar__submenu--open')*/) {
+// // if (window.innerHeight < $('.navbar-ul').prop('scrollHeight') - 44 && $('.navbar__submenu').hasClass('navbar__submenu--open')) {
+// 	 console.log ('двигаем');
+// // 	 // console.log ('obratno');
+// 	if ( $(this).scrollTop() > $('.header-currency').height() /*&& $('.navbar__submenu').hasClass('navbar__submenu--open')*/) {
 	  
-	   // $('.navbar__item-open-btn')[0].scrollIntoView(true);
-	   $(".header-sticky").addClass("header-sticky--stick");
-	   $(".header-sticky").attr('style' , "width: calc(100% - " + $('.navbar').width() + "px )" );
+// 	   // $('.navbar__item-open-btn')[0].scrollIntoView(true);
+// 	   $(".header-sticky").addClass("header-sticky--stick");
+	   
+// 	   $(".header-sticky").attr('style' , "width: calc(100% - " + $('.navbar').width() + "px )" );
 
-	   $(".view").attr('style' , "padding-top: 218px;" );
-	   // $('.navbar__item-main-menu').addClass('navbar__item-main-menu--hide');
-	}  
+// 	   $(".view").attr('style' , "padding-top: 218px;" );
+// 	   // $('.navbar__item-main-menu').addClass('navbar__item-main-menu--hide');
+// 	}  
 
-if ( $(this).scrollTop() == 0) {
-	console.log ('v nol');
-	//заскролили меню в 0
-	// $('.navbar__item-main-menu')[0].scrollIntoView(true);
-	//когда ушли в ноль надо убрать класс вызванный другой функцией
-	$(".navbar__item-open-btn").removeClass("navbar__item--sticky");
+// if ( $(this).scrollTop() == 0) {
+// 	console.log ('v nol');
+// 	//заскролили меню в 0
+// 	// $('.navbar__item-main-menu')[0].scrollIntoView(true);
+// 	//когда ушли в ноль надо убрать класс вызванный другой функцией
+// 	$(".navbar__item-open-btn").removeClass("navbar__item--sticky");
 
-	//убрали липки промежуточный  header
-	$(".header-sticky").removeClass("header-sticky--stick");
-	//вернули относительную ширину
-	$(".header-sticky").attr('style','width: 100%');
-	//
+// 	//убрали липки промежуточный  header
+// 	$(".header-sticky").removeClass("header-sticky--stick");
+// 	//вернули относительную ширину
+// 	$(".header-sticky").attr('style','width: 100%');
+// 	//
 	
 
-	$(".view").removeAttr('style' , "padding-top: 218px;" );
-    // $('.navbar__item-main-menu').removeClass('navbar__item-main-menu--hide');
- }
+// 	$(".view").removeAttr('style' , "padding-top: 218px;" );
+//     // $('.navbar__item-main-menu').removeClass('navbar__item-main-menu--hide');
+//  }
+
 // }
 
 // else {
@@ -60,7 +87,7 @@ if ( $(this).scrollTop() == 0) {
 // }
 
 
-}); //scroll
+// }); //scroll
 
 // $('.navbar__item-open-btn').Stickyfill();
 
