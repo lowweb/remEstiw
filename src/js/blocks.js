1dd6a0fff81b231a8e608e7d8a4326604a81f869
lowweb <37337132+lowweb@users.noreply.github.com>
2018-12-26 00:37:43 +1000

    time

--- src/js/blocks.js
+++ src/js/blocks.js
@@ -20,12 +20,19 @@ if ( $(this).scrollTop() == 0) {
 
  });
 $('.user-popup-menu__title-btn').click(function(){
-	$(this).toggleClass('user-popup-menu__title-btn--up');
+	// $(this).toggleClass('user-popup-menu__title-btn--up');
 	$(this).parents(".user-popup-menu").toggleClass('user-popup-menu--active');
 	$(this).parents(".user-popup-menu").find('.user-popup-menu__add').toggleClass('user-popup-menu__add--active');
 	$(this).toggleClass('btn-rotate180');
 });
 
+$('.timezone-popup-menu__title-btn').click(function(){
+	console.log ('fsdf');
+	$(this).toggleClass('timezone-popup-menu__title-btn--up');
+	$(this).parents(".timezone-popup-menu").toggleClass('timezone-popup-menu--active');
+	$(this).parents(".timezone-popup-menu").find('.timezone-popup-menu__add').toggleClass('timezone-popup-menu__add--active');
+	$(this).toggleClass('btn-rotate180');
+});
 //пример анимации
 $('.user-popup-menu__title-pic').click(function(){
 	$(this).find('.user-popup-menu__title-pic-msg').toggleClass('user-popup-menu__title-pic-msg--active');
