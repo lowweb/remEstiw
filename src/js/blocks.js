ece5f03b5a3293d97fc114655d3346a0922f1e06
lowweb <37337132+lowweb@users.noreply.github.com>
2019-02-10 23:14:33 +1000

    datagrid

--- src/js/blocks.js
+++ src/js/blocks.js
@@ -188,63 +188,53 @@ $("#used-cargo__checkbox").dxCheckBox({
         
 var employees = [{
     "ID": 1,
-    "FirstName": "John",
-    "LastName": "Heart",
-    "Prefix": "Mr.",
-    "Position": "CEO",
-    "Picture": "images/employees/01.png",
-    "BirthDate": "1964/03/16",
-    "HireDate": "1995/01/15",
-    "Notes": "John has been in the Audio/Video industry since 1990. He has led DevAv as its CEO since 2003.\r\n\r\nWhen not working hard as the CEO, John loves to golf and bowl. He once bowled a perfect game of 300.",
-    "Address": "351 S Hill St."
-}, {
-    "ID": 20,
-    "FirstName": "Olivia",
-    "LastName": "Peyton",
-    "Prefix": "Mrs.",
-    "Position": "Sales Assistant",
-    "Picture": "images/employees/09.png",
-    "BirthDate": "1981/06/03",
-    "HireDate": "2012/05/14",
-    "Notes": "Olivia loves to sell. She has been selling DevAV products since 2012. \r\n\r\nOlivia was homecoming queen in high school. She is expecting her first child in 6 months. Good Luck Olivia.",
-    "Address": "807 W Paseo Del Mar"
-}, {
+    "Forma": "ООО",
+    "Name": "Достаточно длинное наименование контрагента «Корпорация Креветка»",
+    "Tel": "+ 7 923 456 78 90",
+    "Email": "mymail@mail.com",
+    "Location": "г. Москва, ул. Стрельникова, 12, офис 23",
+    "Inn": "123456789012",
+    "state": false
+},{
+    "ID": 2,
+    "Forma": "ЗАО",
+    "Name": "Простое название контрагента",
+    "Tel": "+ 7 923 456 78 90",
+    "Email": "mymail@mail.com",
+    "Location": "г.Петропавловск -Камчатский, ул. Бассейная, 666",
+    "Inn": "123456789012",
+    "state": false
+},{
+    "ID": 3,
+    "Forma": "ПАО",
+    "Name": "Достаточно длинное наименование контрагента «Корпорация Креветка»",
+    "Tel": "+ 7 923 456 78 90",
+    "Email": "mymail@mail.com",
+    "Location": "Мохачкала",
+    "Inn": "123456789012",
+    "state": false
+},{
     "ID": 4,
-    "FirstName": "Robert",
-    "LastName": "Reagan",
-    "Prefix": "Mr.",
-    "Position": "CMO",
-    "Picture": "images/employees/03.png",
-    "BirthDate": "1974/09/07",
-    "HireDate": "2002/11/08",
-    "Notes": "Robert was recently voted the CMO of the year by CMO Magazine. He is a proud member of the DevAV Management Team.\r\n\r\nRobert is a championship BBQ chef, so when you get the chance ask him for his secret recipe.",
-    "Address": "4 Westmoreland Pl."
-}, {
+    "Forma": "ООО",
+    "Name": "Достаточно длинное наименование контрагента «Корпорация Креветка»",
+    "Tel": "+ 7 923 456 78 90",
+    "Email": "mymail@mail.com",
+    "Location": "г. Москва, ул. Стрельникова, 12, офис 23",
+    "Inn": "123456789012",
+    "state": false
+},{
     "ID": 5,
-    "FirstName": "Greta",
-    "LastName": "Sims",
-    "Prefix": "Ms.",
-    "Position": "HR Manager",
-    "Picture": "images/employees/04.png",
-    "BirthDate": "1977/11/22",
-    "HireDate": "1998/04/23",
-    "Notes": "Greta has been DevAV's HR Manager since 2003. She joined DevAV from Sonee Corp.\r\n\r\nGreta is currently training for the NYC marathon. Her best marathon time is 4 hours. Go Greta.",
-    "Address": "1700 S Grandview Dr."
-}, {
-    "ID": 6,
-    "FirstName": "Brett",
-    "LastName": "Wade",
-    "Prefix": "Mr.",
-    "Position": "IT Manager",
-    "Picture": "images/employees/05.png",
-    "BirthDate": "1968/12/01",
-    "HireDate": "2009/03/06",
-    "Notes": "Brett came to DevAv from Microsoft and has led our IT department since 2012.\r\n\r\nWhen he is not working hard for DevAV, he coaches Little League (he was a high school pitcher).",
-    "Address": "1120 Old Mill Rd."
-}];
+    "Forma": "ООО",
+    "Name": "Достаточно длинное наименование контрагента «Корпорация Креветка»",
+    "Tel": "+ 7 923 456 78 90",
+    "Email": "mymail@mail.com",
+    "Location": "г. Москва, ул. Стрельникова, 12, офис 23",
+    "Inn": "123456789012",
+    "state": false
+} ];
 
-  $(function(){
-    $("#gridContainer").dxDataGrid({
+
+ var dataGrid =  $("#gridContainer").dxDataGrid({
         dataSource: employees,
         keyExpr: "ID",
         selection: {
@@ -252,29 +242,56 @@ var employees = [{
         },
         hoverStateEnabled: true,
         showBorders: true,
-        columns: [{
-            dataField: "Prefix",
-            caption: "Title",
-            width: 70
-        }, 
-        "FirstName",
-        "LastName", {
-            dataField: "Position",
-            width: 180
-        }, {
-            dataField: "BirthDate",
-            dataType: "date"
-        }, {
-            dataField: "HireDate",
-            dataType: "date"
-        }],
-        onSelectionChanged: function (selectedItems) {
-            var data = selectedItems.selectedRowsData[0];
-            if(data) {
-                $(".employeeNotes").text(data.Notes);
-                $(".employeePhoto").attr("src", data.Picture);
-            }
+        columns: 
+
+        [{
+            dataField: "Forma",
+            caption: "Форма собств.",
+            width: 115
         },
+        {
+            dataField: "Name",
+            caption: "Наименование контрагента",
+             width: 249,
+
+            cellTemplate: function(cellElement, cellInfo) {
+            $('<div class="datagrid__custom-radio-cell">')
+            .appendTo(cellElement)
+            .dxRadioGroup({ 
+                items: [{id: 1}],
+                valueExpr: 'id',
+                value: cellInfo.data.state ? 1 : null,
+                onValueChanged: function(e) {                    
+                    clearState(dataGrid.option("dataSource"), cellInfo.data.id);
+                    dataGrid.refresh();
+                    }
+                })
+                .append($("<a href='#''>"+cellInfo.data.Name+"</a>"))
+             }, 
+
+            cssClass: "datagrid__link-cell"
+        }, 
+        {
+            dataField: "Tel",
+            caption: "Телефон",
+            width: 153
+         
+        },   
+        {
+            dataField: "Email",
+            caption: "Email",
+            width: 144
+        }, 
+        {
+            dataField: "Location",
+            caption: "Местоположение",
+            width: 180
+        }, 
+        {
+            dataField: "Inn",
+            caption: "ИНН",
+            width: 138
+        }],
 
         filterRow: {
             applyFilter: "auto",
@@ -288,8 +305,8 @@ var employees = [{
             },
         showColumnLines: true,
         showRowLines: true,    
-    });
-});
+    }).data("dxDataGrid");
+
 
 $(function () {
     $("#fileUploader").dxFileUploader({
