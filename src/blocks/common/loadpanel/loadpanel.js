e48be25df034650615f350456e143d464f33663c
lowweb <none>
2019-03-11 16:28:37 +1000

    loadpanel

--- src/blocks/common/loadpanel/loadpanel.js
+++ src/blocks/common/loadpanel/loadpanel.js
@@ -1,4 +1,7 @@
 $("#loadPanel").dxLoadPanel({
         closeOnOutsideClick: true,
-        visible: true
+        visible: true,
+        shading: true,
+		shadingColor: "rgba(255,255,255, 0.8)",
+		message: ""
     });
