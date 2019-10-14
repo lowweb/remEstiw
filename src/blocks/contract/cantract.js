8d25ea2f382b3087c75672b45f9bbe541d5b82b8
lowweb <looper@systemTool.local>
2019-10-15 00:43:35 +1000

    contract edit refresh

--- src/blocks/contract/cantract.js
+++ src/blocks/contract/cantract.js
@@ -3,6 +3,11 @@ $('.contract__block-cap').click(function(event) {
 	$(this).parents('.contract__block').toggleClass('contract__block--hide');
 });
 
+$('.contract__mod-block-cap ').click(function(event) {
+
+	$(this).parents('.contract__mod-block').toggleClass('contract__mod-block--hide');
+});
+
 // $('.contract__line-add .btn__rnd').click(function(event) {
 // 	$(this).parents('.contract__line-add').addClass('contract__line-add--act');
 // });
