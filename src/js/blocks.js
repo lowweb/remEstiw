031f957a1f19781657894a7479d6b352800bde92
lowweb <none>
2019-11-20 11:07:15 +1000

    fix arrow on cap

--- src/js/blocks.js
+++ src/js/blocks.js
@@ -4315,6 +4315,62 @@ var now = new Date();
 
 
 
+function textAriaInit (className, width,height) {
+	$("." + className).dxTextArea({
+        spellcheck: false,
+        width: width,
+        height: height,
+        inputAttr: {
+       id: "pseudoElementTextAria__id",
+       class:"input-field__value" 
+   }
+    });
+};
+
+function textAriaInitAutoResize (className, width) {
+	$("." + className).dxTextArea({
+        spellcheck: false,
+        width: width,
+        autoResizeEnabled: true,
+        inputAttr: {
+       id: "pseudoElementTextAria__id",
+       class:"input-field__value" 
+   }
+    });
+};
+
+function textAriaInitAutoResizeminHeight (className, width, minHeight) {
+  $("." + className).dxTextArea({
+        spellcheck: false,
+        width: width,
+        minHeight: minHeight,
+        autoResizeEnabled: true,
+        inputAttr: {
+       id: "pseudoElementTextAria__id",
+       class:"input-field__value" 
+   }
+    });
+};
+
+function textAriaInitAutoResizemaxHeight (className, width, maxHeight) {
+    $("." + className).dxTextArea({
+          spellcheck: false,
+          width: width,
+          minHeight: 36,
+          maxHeight: maxHeight,
+          autoResizeEnabled: true,
+          inputAttr: {
+         id: "pseudoElementTextAria__id",
+         class:"input-field__value" 
+     }
+      });
+  };
+
+
+textAriaInit('pseudoClassTextAria', "auto", 72);
+textAriaInitAutoResize('pseudoClassTextAriaAutoHeight', "auto");
+textAriaInitAutoResizeminHeight('pseudoClassTextAriaAutoHeight72', "auto" , 72);
+textAriaInitAutoResizemaxHeight('pseudoClassTextAriaAutoHeightMax108', "auto" , 108);
 function selectBoxInitForId (idElement, dataS, inputAttrId, inputAttrVal ) {
 $("#" + idElement).dxSelectBox({
         dataSource: dataS,
@@ -4383,62 +4439,6 @@ function selectBoxForChat (classElement, dataS, inputAttrId, inputAttrVal ) {
 
 
 
-function textAriaInit (className, width,height) {
-	$("." + className).dxTextArea({
-        spellcheck: false,
-        width: width,
-        height: height,
-        inputAttr: {
-       id: "pseudoElementTextAria__id",
-       class:"input-field__value" 
-   }
-    });
-};
-
-function textAriaInitAutoResize (className, width) {
-	$("." + className).dxTextArea({
-        spellcheck: false,
-        width: width,
-        autoResizeEnabled: true,
-        inputAttr: {
-       id: "pseudoElementTextAria__id",
-       class:"input-field__value" 
-   }
-    });
-};
-
-function textAriaInitAutoResizeminHeight (className, width, minHeight) {
-  $("." + className).dxTextArea({
-        spellcheck: false,
-        width: width,
-        minHeight: minHeight,
-        autoResizeEnabled: true,
-        inputAttr: {
-       id: "pseudoElementTextAria__id",
-       class:"input-field__value" 
-   }
-    });
-};
-
-function textAriaInitAutoResizemaxHeight (className, width, maxHeight) {
-    $("." + className).dxTextArea({
-          spellcheck: false,
-          width: width,
-          minHeight: 36,
-          maxHeight: maxHeight,
-          autoResizeEnabled: true,
-          inputAttr: {
-         id: "pseudoElementTextAria__id",
-         class:"input-field__value" 
-     }
-      });
-  };
-
-
-textAriaInit('pseudoClassTextAria', "auto", 72);
-textAriaInitAutoResize('pseudoClassTextAriaAutoHeight', "auto");
-textAriaInitAutoResizeminHeight('pseudoClassTextAriaAutoHeight72', "auto" , 72);
-textAriaInitAutoResizemaxHeight('pseudoClassTextAriaAutoHeightMax108', "auto" , 108);
  $(".popup__info-newmsg").dxToast({
         message: "У вас новое сообщение по запросу (пример всплывающего уведомления)",
         displayTime: 300000,
